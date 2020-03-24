82ba4295bc2be97cf011456f43852de972b3bf4a
AhmedMohamedSaleh <43453197+AhmedMohamedSaleh@users.noreply.github.com>
2020-03-25 01:12:06 +0200

    Adding android project
    creating new android project

---
+++ app/src/main/java/com/example/android/youtube_play_native_video_github/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.youtube_play_native_video_github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
